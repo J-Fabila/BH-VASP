424d1a8b05746ba59e453aff020063348f37d183
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-01-03 16:23:51 -0600

    Update queue.sh

--- queue.sh
+++ queue.sh
@@ -2,13 +2,14 @@
 #BSUB -q q_residual
 #BSUB -oo output
 #BSUB -eo error
-#BSUB -n 32
+#BSUB -n 128
 module load use.own
 module load vasp/5.4.4
 #BSUB -R "span[ptile=32]"
 #BSUB -m "g3_a g3_b"
-#./continue.sh 2> ERRORES
-./pruebainit.sh 2> ERRORES > OUTPUT
-#./Au1Ir1/aux.sh
+#BSUB -J Ir5
+
+#./continue.sh 2>> Ir5.err >> Ir5.out
+./basin_hopping.sh 2>> Ir5.err >> Ir5.out
 
 
